c78436cada980381c3d32293b8a8f7a47681b7b5
Kamil Waligóra <k.waligora37@gmail.com>
2021-09-05 14:31:23 +0200

    Add simple move to location

---
+++ Source/Unreal_RPG/RPG_PlayerController.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RPG_PlayerController.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+ARPG_PlayerController::ARPG_PlayerController()
+{
+	bShowMouseCursor = true;
+}
+
+void ARPG_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("SetDestination", IE_Pressed, this, &ARPG_PlayerController::OnSetDestinationPressed);
+}
+
+void ARPG_PlayerController::OnSetDestinationPressed()
+{
+	FHitResult HitResult;
+	GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+	if(HitResult.bBlockingHit)
+	{
+		MoveToLocation(HitResult.ImpactPoint);
+	}
+}
+
+void ARPG_PlayerController::MoveToLocation(FVector Location)
+{
+	APawn* PlayerPawn = GetPawn();
+	if(PlayerPawn)
+	{
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, Location);
+	}
+}
---
+++ Source/Unreal_RPG/RPG_PlayerController.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RPG_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_RPG_API ARPG_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ARPG_PlayerController();
+
+protected:
+	virtual void SetupInputComponent() override;
+	void OnSetDestinationPressed();
+	void MoveToLocation(FVector Location);
+};
